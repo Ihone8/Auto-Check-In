fd8e4f7f6261cd0778accb486122659a79769f7c
json <34541083+Ihone8@users.noreply.github.com>
2018-09-19 15:46:03 +0800

    Update myfirst.py

--- myfirst.py
+++ myfirst.py
@@ -9,31 +9,6 @@ import re
 from threading import Timer
 from datetime import datetime
 
-class MyTimer( object ):
-
-    def __init__( self, start_time, interval, callback_proc, args=None, kwargs=None ):
-
-        self.__timer = None
-        self.__start_time = start_time
-        self.__interval = interval
-        self.__callback_pro = callback_proc
-        self.__args = args if args is not None else []
-        self.__kwargs = kwargs if kwargs is not None else {}
-
-    def exec_callback( self, args=None, kwargs=None ):
-        self.__callback_pro( *self.__args, **self.__kwargs )
-        self.__timer = Timer( self.__interval, self.exec_callback )
-        self.__timer.start()
-
-    def start( self ):
-        interval = self.__interval - ( datetime.now().timestamp() - self.__start_time.timestamp() )
-        #print( interval )
-        self.__timer = Timer( interval, self.exec_callback )
-        self.__timer.start()
-
-    def cancel( self ):
-        self.__timer.cancel()
-        self.__timer = None
 
 
 
@@ -87,11 +62,9 @@ def Sign():
 
 
 if __name__== '__main__':
+    # 死循环  60 秒一次
         while True:
             Sign()
             time.sleep(60)
-        #start = datetime.now().replace(minute=3, second=0, microsecond=0)
-        #tmr = MyTimer(start, 60 * 60, Sign())
-        #tmr.start()
-        #tmr.cancel()
+      
 
